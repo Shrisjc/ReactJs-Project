4df06d082b9d0ae153919efc1b537406834f9609
Shridhar <jadhavshridhar787@gmail.com>
2023-07-19 17:28:48 +0530

    First Change

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar title="TextUtils" />
+      <div className="container my-3">
+        <TextForm heading="Enter the text to analyze" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/TextForm.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+  const handleUPClick = () => {
+    console.log("Click up case");
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+  const handleOnChange = (event) => {
+    console.log("onChange button click");
+    setText(event.target.value);
+  };
+  const [text, setText] = useState("Enter text here");
+  // text = "new text" // wrong way to change the state
+  //setText("shri"); // correct way to change state
+  return (
+    <div>
+      <h1>{props.heading}</h1>
+      <div className="mb-3">
+        <label htmlFor="myBox" className="form-label"></label>
+        <textarea
+          className="form-control"
+          value={text}
+          onChange={handleOnChange}
+          id="myBox"
+          rows="8"
+        ></textarea>
+      </div>
+      <button className="btn btn-primary" onClick={handleUPClick}>
+        Conver to Uppercase
+      </button>
+    </div>
+  );
+}
